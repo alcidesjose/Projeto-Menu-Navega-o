910b27c8ee7922f74425946f0bac0166b41f445e
JUNIOR <iqalcides@gmail.com>
2022-05-19 15:16:54 -0300

    Tentando resolver questão da função so responder com onclick

---
+++ script.js
@@ -0,0 +1,51 @@
+// let oPen = document.querySelector('.menu-area');
+
+
+//  function oPencloSe() {
+
+//   oPen.style.width === "0px" ? oPen.style.width = "250px" : oPen.style.width = "0px"
+
+// }; **** Essa é a que funciona, porém somente usando onclick *****
+
+
+
+let oPen = document.querySelector('.menu-area');
+
+
+ oPen.addEventListener('click',function oPencloSe() {
+
+  oPen.style.width === "0px" ? oPen.style.width = "250px" : oPen.style.width = "0px"
+
+});
+
+
+
+
+
+
+
+
+// let menuArea= document.querySelector('.menu-area');
+
+
+// menuArea.addEventListener('click',function menuToggle() {
+
+//   let area = document.querySelector('.menua-opener')
+//       if (menuArea.classList.contains('menu-opened') == true) {
+//           menuArea.classList.remove('menu-opened');
+//       } else {
+//           menuArea.classList.add('menu-opened');
+//       }
+
+
+//     }
+//   );
+
+
+
+
+
+
+
+
+
